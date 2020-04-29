ba6a16bb875677331f4540de1afd63c90335fcab
David Jones <kingdavidjones@Davids-MacBook-Pro.local>
2020-04-29 12:04:36 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Header from "./Components/Header"
+import MemeGenerator from "./Components/MemeGenerator"
+
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <MemeGenerator />
+
+        </div>)
+}
+
+
+
+
+export default App
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+function Header(){
+    return(
+        <div>
+            <header>
+                <img 
+                    src="https://hips.hearstapps.com/esq.h-cdn.co/assets/17/03/1484751145-chappelle-rick-james.jpg" 
+                    alt="Problem?"
+                />
+                <p>David's Meme Generator</p>
+            </header>
+        </div>
+        )
+}
+
+
+
+
+
+export default Header
